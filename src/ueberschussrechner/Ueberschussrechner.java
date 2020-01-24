442d1aee44ff9fc6da5de94b86af7716f6671801
bibi <bianc@DESKTOP-9QI8JK4.fritz.box>
2020-01-24 17:25:31 +0100

    Kommentare hinzugefügt

--- src/ueberschussrechner/Ueberschussrechner.java
+++ src/ueberschussrechner/Ueberschussrechner.java
@@ -4,13 +4,17 @@ public class Ueberschussrechner {
 
     public static void main(String[] args) {
 
+        //Instanzierung der Klassen
         GUI gui = new GUI();
         Dateiverarbeitung dateiverarbeitung = new Dateiverarbeitung();
         FileHandler fileHandler = new FileHandler(dateiverarbeitung);
 
+        //Gui sichtbar machen
         gui.setVisible(true);
+        //setter aufrufen
         gui.setDateiverarbeitung(dateiverarbeitung);
         gui.setFileHandler(fileHandler);
+        //Sortierung
         gui.Buchungstabelle.setAutoCreateRowSorter(true);
     }
 }
